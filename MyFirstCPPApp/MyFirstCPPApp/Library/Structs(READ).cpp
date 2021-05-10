30d0259a8b747de32393ce386e23a282258c8c7a
Ivan Kazantsev <vanskazancev@gmail.com>
2021-05-10 17:50:54 +0500

    added structs basics

---
+++ MyFirstCPPApp/MyFirstCPPApp/Library/Structs(READ).cpp
@@ -0,0 +1,34 @@
+//
+//  Structs(READ).cpp
+//  MyFirstCPPApp
+//
+//  Created by Иван Казанцев on 10.05.2021.
+//
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+struct Date {
+    int year;
+    
+};
+
+struct Auto {
+    int wheels;
+    float speed;
+    char colour;
+    
+    Date manufacturingYear;
+};
+
+//Auto skoda;
+//skoda.wheels = 4;
+//skoda.speed = 300;
+//skoda.colour = 'r';
+//skoda.manufacturingYear.year = 2015;
+
+//Auto audi = {4, 300, 'r'};
+
+//cout << audi.speed << endl;
+
